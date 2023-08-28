1fb65a5aeb56bb52ebda33d4cb0ed0381c01ad09
FiodhyAN <ardhito917@gmail.com>
2023-08-28 20:25:09 +0700

    change folder name

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {StatusBar} from 'react-native';
-import {AuthContext, AuthProvider} from './src/context/AuthContext';
-import Navigation from './src/component/Navigation';
+import {AuthProvider} from './src/context/AuthContext';
+import Navigation from './src/navigation/Navigation';
 
 export default function App() {
   return (
