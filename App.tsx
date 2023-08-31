4b479165b48d30f1f6a02c151a1250ddc6e60696
FiodhyAN <ardhito917@gmail.com>
2023-08-31 11:45:03 +0700

    set connection status

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
-import {AuthProvider} from './src/context/AuthContext';
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'react-native';
+import { AuthProvider } from './src/context/AuthContext';
 import Navigation from './src/navigation/Navigation';
+import NetInfo from '@react-native-community/netinfo';
+import SplashScreen from './src/screens/SplashScreen';
 
 export default function App() {
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected ?? false);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
-    <AuthProvider>
-      <StatusBar hidden={true} />
-      <Navigation />
-    </AuthProvider>
+    isConnected ? (
+      <AuthProvider>
+        <StatusBar hidden={true} />
+        <Navigation />
+      </AuthProvider>
+    ) : (
+      <SplashScreen />
+    )
   );
 }
